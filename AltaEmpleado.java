195a7cd768c5dbf09348f8bcac8fc825544a792f
EduardoRufinoYepez <72160178+EduardoRufinoYepez@users.noreply.github.com>
2021-03-01 17:28:33 -0600

    Add files via upload

--- AltaEmpleado.java
+++ AltaEmpleado.java
@@ -5,6 +5,8 @@
  */
 package mx.unam.aragon.fes.gui;
 
+import mx.unam.aragon.fes.Empleado;
+
 /**
  *
  * @author YO claro que si
@@ -63,8 +65,11 @@ public class AltaEmpleado extends javax.swing.JFrame {
         jButton2 = new javax.swing.JButton();
         jButton3 = new javax.swing.JButton();
         jButton4 = new javax.swing.JButton();
+        jButton5 = new javax.swing.JButton();
+        jButton6 = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setEnabled(false);
 
         jTabbedPane1.setBackground(new java.awt.Color(102, 255, 102));
         jTabbedPane1.setBorder(javax.swing.BorderFactory.createTitledBorder("Registro de empleado"));
@@ -72,6 +77,11 @@ public class AltaEmpleado extends javax.swing.JFrame {
         jLabel1.setText("Nombre:");
 
         jTextField1.setText("jTextField1");
+        jTextField1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jTextField1ActionPerformed(evt);
+            }
+        });
 
         jTextField2.setText("jTextField2");
 
@@ -297,13 +307,27 @@ public class AltaEmpleado extends javax.swing.JFrame {
 
         jButton4.setText(">>");
 
+        jButton5.setText("Nuevo registro");
+        jButton5.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                jButton5MouseClicked(evt);
+            }
+        });
+
+        jButton6.setText("Enviar");
+        jButton6.setEnabled(false);
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addComponent(jTabbedPane1)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addContainerGap()
+                .addComponent(jButton5)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jButton6, javax.swing.GroupLayout.PREFERRED_SIZE, 73, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addComponent(jButton3, javax.swing.GroupLayout.PREFERRED_SIZE, 73, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(jButton4, javax.swing.GroupLayout.PREFERRED_SIZE, 73, javax.swing.GroupLayout.PREFERRED_SIZE)
@@ -322,13 +346,54 @@ public class AltaEmpleado extends javax.swing.JFrame {
                     .addComponent(jButton1)
                     .addComponent(jButton2)
                     .addComponent(jButton3)
-                    .addComponent(jButton4))
+                    .addComponent(jButton4)
+                    .addComponent(jButton5)
+                    .addComponent(jButton6))
                 .addContainerGap())
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
+    private void jButton5MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jButton5MouseClicked
+        // TODO add your handling code here:
+        System.out.println("Nuevo empleado!!!");
+        this.LimpiarFormulario();
+        
+        Empleado emp= new Empleado();
+        emp.setNombre( this.jTextField1.getText());
+        emp.setApPaterno(this.jTextField1.getText());
+        emp.setApMaterno(this.jTextField1.getText());
+        emp.setEdad(Integer.parseInt(jTextField4.getText()));
+        emp.setCurp(jTextField5.getText());
+        emp.getDomicilio().setCalle(this.jTextField6.getText());
+    }//GEN-LAST:event_jButton5MouseClicked
+
+    private void jTextField1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jTextField1ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jTextField1ActionPerformed
+    private void LimpiarFormulario(){
+         this.jTextField1.setText("");
+         this.jTextField2.setText("");
+         this.jTextField3.setText("");
+         this.jTextField4.setText("");
+         this.jTextField5.setText("");
+         this.jTextField6.setText("");
+         this.jTextField7.setText("");
+         this.jTextField8.setText("");
+         this.jTextField9.setText("");
+         this.jTextField10.setText("");
+         this.jTextField11.setText("");
+         this.jTextField11.setColumns(10);
+         this.jTextField12.setText("");
+         this.jTextField12.setColumns(10);
+         this.jTextField13.setText("");
+         this.jTextField14.setText("");
+         this.jTextField15.setText("");
+         this.jTextField15.setColumns(10);
+         
+         
+    }
     /**
      * @param args the command line arguments
      */
@@ -369,6 +434,8 @@ public class AltaEmpleado extends javax.swing.JFrame {
     private javax.swing.JButton jButton2;
     private javax.swing.JButton jButton3;
     private javax.swing.JButton jButton4;
+    private javax.swing.JButton jButton5;
+    private javax.swing.JButton jButton6;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel10;
     private javax.swing.JLabel jLabel11;
